dd518f369d8fddacb8fab5b7142dc9534cf7ed71
Norbert Hajagos <hajagosnorbi@gmail.com>
2018-12-31 17:41:04 +0100

    Demonstrálom a gittet

--- restaurant_learn/config.py
+++ restaurant_learn/config.py
@@ -7,5 +7,5 @@ database_file = f'sqlite:///{os.path.join(project_dir, "restaurantmenu.db")}'
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY').encode()
     SQLALCHEMY_DATABASE_URI = database_file
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SEND_FILE_MAX_AGE_DEFAULT = 0
